a44df94ebc20571ab92b5ed8dd20a57608ba0500
Pratyush615 <75452860+Pratyush615@users.noreply.github.com>
2023-10-14 11:54:35 -0400

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent("player-movement", {
+  init: function () {
+    this.walk();
+  },
+  walk: function () {
+    window.addEventListener("keydown", (e) => {
+      if (
+        e.key === "ArrowUp" ||
+        e.key === "ArrowRight" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowDown"
+      ) {
+   //add the sound for movement
+        console.log('#')
+        var entity = document.querySelector('#sound1')
+        entity.components.sound.playSound()
+      }
+    });
+  },
+});
+
